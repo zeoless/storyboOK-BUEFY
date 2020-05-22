c2c3ac1911dc99e8b2881580919965cb87074a68
zeoless <125026834+zeoless@users.noreply.github.com>
2020-05-22 15:02:32 +0000

    CV updated

--- internal/engine/postgresql/contrib/citext.go
+++ internal/engine/postgresql/contrib/citext.go
@@ -231,4 +231,228 @@ var funcsCitext = []*catalog.Function{
 				Type: &ast.TypeName{Name: "cstring"},
 			},
 		},
-		ReturnType: &ast.TypeName{Name:
+		ReturnType: &ast.TypeName{Name: "citext"},
+	},
+	{
+		Name: "citextout",
+		Args: []*catalog.Argument{
+			{
+				Type: &ast.TypeName{Name: "citext"},
+			},
+		},
+		ReturnType: &ast.TypeName{Name: "cstring"},
+	},
+	{
+		Name: "citextsend",
+		Args: []*catalog.Argument{
+			{
+				Type: &ast.TypeName{Name: "citext"},
+			},
+		},
+		ReturnType: &ast.TypeName{Name: "bytea"},
+	},
+	{
+		Name: "max",
+		Args: []*catalog.Argument{
+			{
+				Type: &ast.TypeName{Name: "citext"},
+			},
+		},
+		ReturnType: &ast.TypeName{Name: "citext"},
+	},
+	{
+		Name: "min",
+		Args: []*catalog.Argument{
+			{
+				Type: &ast.TypeName{Name: "citext"},
+			},
+		},
+		ReturnType: &ast.TypeName{Name: "citext"},
+	},
+	{
+		Name: "regexp_match",
+		Args: []*catalog.Argument{
+			{
+				Type: &ast.TypeName{Name: "citext"},
+			},
+			{
+				Type: &ast.TypeName{Name: "citext"},
+			},
+		},
+		ReturnType: &ast.TypeName{Name: "text[]"},
+	},
+	{
+		Name: "regexp_match",
+		Args: []*catalog.Argument{
+			{
+				Type: &ast.TypeName{Name: "citext"},
+			},
+			{
+				Type: &ast.TypeName{Name: "citext"},
+			},
+			{
+				Type: &ast.TypeName{Name: "text"},
+			},
+		},
+		ReturnType: &ast.TypeName{Name: "text[]"},
+	},
+	{
+		Name: "regexp_matches",
+		Args: []*catalog.Argument{
+			{
+				Type: &ast.TypeName{Name: "citext"},
+			},
+			{
+				Type: &ast.TypeName{Name: "citext"},
+			},
+		},
+		ReturnType: &ast.TypeName{Name: "text[]"},
+	},
+	{
+		Name: "regexp_matches",
+		Args: []*catalog.Argument{
+			{
+				Type: &ast.TypeName{Name: "citext"},
+			},
+			{
+				Type: &ast.TypeName{Name: "citext"},
+			},
+			{
+				Type: &ast.TypeName{Name: "text"},
+			},
+		},
+		ReturnType: &ast.TypeName{Name: "text[]"},
+	},
+	{
+		Name: "regexp_replace",
+		Args: []*catalog.Argument{
+			{
+				Type: &ast.TypeName{Name: "citext"},
+			},
+			{
+				Type: &ast.TypeName{Name: "citext"},
+			},
+			{
+				Type: &ast.TypeName{Name: "text"},
+			},
+		},
+		ReturnType: &ast.TypeName{Name: "text"},
+	},
+	{
+		Name: "regexp_replace",
+		Args: []*catalog.Argument{
+			{
+				Type: &ast.TypeName{Name: "citext"},
+			},
+			{
+				Type: &ast.TypeName{Name: "citext"},
+			},
+			{
+				Type: &ast.TypeName{Name: "text"},
+			},
+			{
+				Type: &ast.TypeName{Name: "text"},
+			},
+		},
+		ReturnType: &ast.TypeName{Name: "text"},
+	},
+	{
+		Name: "regexp_split_to_array",
+		Args: []*catalog.Argument{
+			{
+				Type: &ast.TypeName{Name: "citext"},
+			},
+			{
+				Type: &ast.TypeName{Name: "citext"},
+			},
+		},
+		ReturnType: &ast.TypeName{Name: "text[]"},
+	},
+	{
+		Name: "regexp_split_to_array",
+		Args: []*catalog.Argument{
+			{
+				Type: &ast.TypeName{Name: "citext"},
+			},
+			{
+				Type: &ast.TypeName{Name: "citext"},
+			},
+			{
+				Type: &ast.TypeName{Name: "text"},
+			},
+		},
+		ReturnType: &ast.TypeName{Name: "text[]"},
+	},
+	{
+		Name: "regexp_split_to_table",
+		Args: []*catalog.Argument{
+			{
+				Type: &ast.TypeName{Name: "citext"},
+			},
+			{
+				Type: &ast.TypeName{Name: "citext"},
+			},
+		},
+		ReturnType: &ast.TypeName{Name: "text"},
+	},
+	{
+		Name: "regexp_split_to_table",
+		Args: []*catalog.Argument{
+			{
+				Type: &ast.TypeName{Name: "citext"},
+			},
+			{
+				Type: &ast.TypeName{Name: "citext"},
+			},
+			{
+				Type: &ast.TypeName{Name: "text"},
+			},
+		},
+		ReturnType: &ast.TypeName{Name: "text"},
+	},
+	{
+		Name: "replace",
+		Args: []*catalog.Argument{
+			{
+				Type: &ast.TypeName{Name: "citext"},
+			},
+			{
+				Type: &ast.TypeName{Name: "citext"},
+			},
+			{
+				Type: &ast.TypeName{Name: "citext"},
+			},
+		},
+		ReturnType: &ast.TypeName{Name: "text"},
+	},
+	{
+		Name: "split_part",
+		Args: []*catalog.Argument{
+			{
+				Type: &ast.TypeName{Name: "citext"},
+			},
+			{
+				Type: &ast.TypeName{Name: "citext"},
+			},
+			{
+				Type: &ast.TypeName{Name: "integer"},
+			},
+		},
+		ReturnType: &ast.TypeName{Name: "text"},
+	},
+	{
+		Name: "strpos",
+		Args: []*catalog.Argument{
+			{
+				Type: &ast.TypeName{Name: "citext"},
+			},
+			{
+				Type: &ast.TypeName{Name: "citext"},
+			},
+		},
+		ReturnType: &ast.TypeName{Name: "integer"},
+	},
+	{
+		Name: "texticlike",
+		Args: []*catalog.Argument{
+			
