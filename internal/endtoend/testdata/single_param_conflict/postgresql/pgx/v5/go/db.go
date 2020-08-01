70c7444475ca9627a4f93c1205566cbe31f4af7a
zeoless <125026834+zeoless@users.noreply.github.com>
2020-08-01 12:25:52 +0000

    Add Procfile for heroku

--- internal/endtoend/testdata/single_param_conflict/postgresql/pgx/v5/go/db.go
+++ internal/endtoend/testdata/single_param_conflict/postgresql/pgx/v5/go/db.go
@@ -11,4 +11,22 @@ import (
 	"github.com/jackc/pgx/v5/pgconn"
 )
 
-type DBTX inter
+type DBTX interface {
+	Exec(context.Context, string, ...interface{}) (pgconn.CommandTag, error)
+	Query(context.Context, string, ...interface{}) (pgx.Rows, error)
+	QueryRow(context.Context, string, ...interface{}) pgx.Row
+}
+
+func New(db DBTX) *Queries {
+	return &Queries{db: db}
+}
+
+type Queries struct {
+	db DBTX
+}
+
+func (q *Queries) WithTx(tx pgx.Tx) *Queries {
+	return &Queries{
+		db: tx,
+	}
+}
