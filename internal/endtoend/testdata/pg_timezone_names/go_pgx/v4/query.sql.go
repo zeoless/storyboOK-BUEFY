1efcb902830a257dad47118a030f77c50fd2f771
zeoless <125026834+zeoless@users.noreply.github.com>
2020-11-14 12:48:23 +0000

    Installing ttfautohint from ppa:fontforge/fontforge repository

--- internal/endtoend/testdata/pg_timezone_names/go_pgx/v4/query.sql.go
+++ internal/endtoend/testdata/pg_timezone_names/go_pgx/v4/query.sql.go
@@ -64,4 +64,37 @@ func (q *Queries) GetTables(ctx context.Context) ([]string, error) {
 }
 
 const getTimezones = `-- name: GetTimezones :many
-SELECT name, abbrev, 
+SELECT name, abbrev, utc_offset, is_dst from pg_catalog.pg_timezone_names
+`
+
+type GetTimezonesRow struct {
+	Name      sql.NullString
+	Abbrev    sql.NullString
+	UtcOffset sql.NullInt64
+	IsDst     sql.NullBool
+}
+
+func (q *Queries) GetTimezones(ctx context.Context) ([]GetTimezonesRow, error) {
+	rows, err := q.db.Query(ctx, getTimezones)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+	var items []GetTimezonesRow
+	for rows.Next() {
+		var i GetTimezonesRow
+		if err := rows.Scan(
+			&i.Name,
+			&i.Abbrev,
+			&i.UtcOffset,
+			&i.IsDst,
+		); err != nil {
+			return nil, err
+		}
+		items = append(items, i)
+	}
+	if err := rows.Err(); err != nil {
+		return nil, err
+	}
+	return items, nil
+}
---
+++ internal/endtoend/testdata/star_expansion/postgresql/stdlib/query.sql
@@ -0,0 +1,5 @@
+
+CREATE TABLE foo (a text, b text);
+
+-- name: StarExpansion :many
+SELECT *, *, foo.* FROM foo;
