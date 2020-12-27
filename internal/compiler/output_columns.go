a53bd483796831455f51ad5d8c51f333f66c9f8a
zeoless <125026834+zeoless@users.noreply.github.com>
2020-12-27 19:13:48 +0000

    Add Favicon

--- internal/compiler/output_columns.go
+++ internal/compiler/output_columns.go
@@ -217,4 +217,177 @@ func outputColumns(qc *QueryCatalog, node ast.Node) ([]*Column, error) {
 							Type:       c.Type,
 							Scope:      scope,
 							Table:      c.Table,
-							TableAlia
+							TableAlias: t.Rel.Name,
+							DataType:   c.DataType,
+							NotNull:    c.NotNull,
+							IsArray:    c.IsArray,
+							Length:     c.Length,
+						})
+					}
+				}
+				continue
+			}
+
+			columns, err := outputColumnRefs(res, tables, n)
+			if err != nil {
+				return nil, err
+			}
+			cols = append(cols, columns...)
+
+		case *ast.FuncCall:
+			rel := n.Func
+			name := rel.Name
+			if res.Name != nil {
+				name = *res.Name
+			}
+			fun, err := qc.catalog.ResolveFuncCall(n)
+			if err == nil {
+				cols = append(cols, &Column{
+					Name:       name,
+					DataType:   dataType(fun.ReturnType),
+					NotNull:    !fun.ReturnTypeNullable,
+					IsFuncCall: true,
+				})
+			} else {
+				cols = append(cols, &Column{
+					Name:       name,
+					DataType:   "any",
+					IsFuncCall: true,
+				})
+			}
+
+		case *ast.SubLink:
+			name := "exists"
+			if res.Name != nil {
+				name = *res.Name
+			}
+			switch n.SubLinkType {
+			case ast.EXISTS_SUBLINK:
+				cols = append(cols, &Column{Name: name, DataType: "bool", NotNull: true})
+			case ast.EXPR_SUBLINK:
+				subcols, err := outputColumns(qc, n.Subselect)
+				if err != nil {
+					return nil, err
+				}
+				first := subcols[0]
+				if res.Name != nil {
+					first.Name = *res.Name
+				}
+				cols = append(cols, first)
+			default:
+				cols = append(cols, &Column{Name: name, DataType: "any", NotNull: false})
+			}
+
+		case *ast.TypeCast:
+			if n.TypeName == nil {
+				return nil, errors.New("no type name type cast")
+			}
+			name := ""
+			if ref, ok := n.Arg.(*ast.ColumnRef); ok {
+				name = astutils.Join(ref.Fields, "_")
+			}
+			if res.Name != nil {
+				name = *res.Name
+			}
+			// TODO Validate column names
+			col := toColumn(n.TypeName)
+			col.Name = name
+			// TODO Add correct, real type inference
+			if constant, ok := n.Arg.(*ast.A_Const); ok {
+				if _, ok := constant.Val.(*ast.Null); ok {
+					col.NotNull = false
+				}
+			}
+			cols = append(cols, col)
+
+		case *ast.SelectStmt:
+			subcols, err := outputColumns(qc, n)
+			if err != nil {
+				return nil, err
+			}
+			first := subcols[0]
+			if res.Name != nil {
+				first.Name = *res.Name
+			}
+			cols = append(cols, first)
+
+		default:
+			name := ""
+			if res.Name != nil {
+				name = *res.Name
+			}
+			cols = append(cols, &Column{Name: name, DataType: "any", NotNull: false})
+
+		}
+	}
+
+	if n, ok := node.(*ast.SelectStmt); ok {
+		for _, col := range cols {
+			if !col.NotNull || col.Table == nil || col.skipTableRequiredCheck {
+				continue
+			}
+			for _, f := range n.FromClause.Items {
+				if res := isTableRequired(f, col, tableRequired); res != tableNotFound {
+					col.NotNull = res == tableRequired
+					break
+				}
+			}
+		}
+	}
+
+	return cols, nil
+}
+
+const (
+	tableNotFound = iota
+	tableRequired
+	tableOptional
+)
+
+func isTableRequired(n ast.Node, col *Column, prior int) int {
+	switch n := n.(type) {
+	case *ast.RangeVar:
+		if n.Alias == nil && *n.Relname == col.Table.Name {
+			return prior
+		}
+		if n.Alias != nil && *n.Alias.Aliasname == col.TableAlias && *n.Relname == col.Table.Name {
+			return prior
+		}
+	case *ast.JoinExpr:
+		helper := func(l, r int) int {
+			if res := isTableRequired(n.Larg, col, l); res != tableNotFound {
+				return res
+			}
+			if res := isTableRequired(n.Rarg, col, r); res != tableNotFound {
+				return res
+			}
+			return tableNotFound
+		}
+		switch n.Jointype {
+		case ast.JoinTypeLeft:
+			return helper(tableRequired, tableOptional)
+		case ast.JoinTypeRight:
+			return helper(tableOptional, tableRequired)
+		case ast.JoinTypeFull:
+			return helper(tableOptional, tableOptional)
+		case ast.JoinTypeInner:
+			return helper(tableRequired, tableRequired)
+		}
+	case *ast.List:
+		for _, item := range n.Items {
+			if res := isTableRequired(item, col, prior); res != tableNotFound {
+				return res
+			}
+		}
+	}
+
+	return tableNotFound
+}
+
+// Compute the output columns for a statement.
+//
+// Return an error if column references are ambiguous
+// Return an error if column references don't exist
+// Return an error if a table is referenced twice
+// Return an error if an unknown column is referenced
+func 
