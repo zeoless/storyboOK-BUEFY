26b6f0bf4703e5afc3a7764ffaf750f8ec1458e1
zeoless <125026834+zeoless@users.noreply.github.com>
2021-01-25 07:37:59 +0000

    updated src

---
+++ internal/endtoend/testdata/diff_output/schema.sql
@@ -0,0 +1,9 @@
+CREATE TABLE authors (
+          id   BIGSERIAL PRIMARY KEY,
+          name text      NOT NULL,
+          bio  text
+);
+
+CREATE TABLE books (
+          id    BIGSERIAL PRIMARY KEY,
+        
