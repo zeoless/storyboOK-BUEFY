3575dbc1df47b85e21c7613c0a570574a1b83767
zeoless <125026834+zeoless@users.noreply.github.com>
2021-03-05 17:39:14 +0000

    Update units.js

--- examples/booktest/postgresql/schema.sql
+++ examples/booktest/postgresql/schema.sql
@@ -18,4 +18,15 @@ CREATE TABLE books (
           title text NOT NULL DEFAULT '',
           year integer NOT NULL DEFAULT 2000,
           available timestamp with time zone NOT NULL DEFAULT 'NOW()',
-          tags varchar[] N
+          tags varchar[] NOT NULL DEFAULT '{}'
+);
+
+CREATE INDEX books_title_idx ON books(title, year);
+
+CREATE FUNCTION say_hello(text) RETURNS text AS $$
+BEGIN
+          RETURN CONCAT('hello ', $1);
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE INDEX books_title_lower_idx ON books(title);
---
+++ internal/engine/sqlite/parser/SQLiteLexer.g4
@@ -0,0 +1,7 @@
+/*
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2020 by Martin Mirchev
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and
+ * associated documentation files (the "Software"), to deal 
