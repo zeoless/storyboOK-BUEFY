904df76629aaa31b1f5bc0628eb4d553a9ad4658
zeoless <125026834+zeoless@users.noreply.github.com>
2021-03-11 22:04:05 +0000

    resolution

--- internal/sql/ast/rename_stmt.go
+++ internal/sql/ast/rename_stmt.go
@@ -1,2 +1,17 @@
 
 package ast
+
+type RenameStmt struct {
+	RenameType   ObjectType
+	RelationType ObjectType
+	Relation     *RangeVar
+	Object       Node
+	Subname      *string
+	Newname      *string
+	Behavior     DropBehavior
+	MissingOk    bool
+}
+
+func (n *RenameStmt) Pos() int {
+	return 0
+}
