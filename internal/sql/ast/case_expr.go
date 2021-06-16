98b3f539b00d6f7de771ced41fe62006e85fb74c
zeoless <125026834+zeoless@users.noreply.github.com>
2021-06-16 20:33:42 +0000

    Logging cleanup.

---
+++ internal/sql/ast/case_expr.go
@@ -0,0 +1,16 @@
+
+package ast
+
+type CaseExpr struct {
+	Xpr        Node
+	Casetype   Oid
+	Casecollid Oid
+	Arg        Node
+	Args       *List
+	Defresult  Node
+	Location   int
+}
+
+func (n *CaseExpr) Pos() int {
+	return n.Location
+}
---
+++ internal/sql/ast/object_type.go
@@ -0,0 +1,8 @@
+
+package ast
+
+type ObjectType uint
+
+func (n *ObjectType) Pos() int {
+	return 0
+}
