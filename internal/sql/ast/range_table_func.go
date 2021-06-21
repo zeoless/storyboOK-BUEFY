13d2f9d16a6cfd8d1899c3678707814ed38514d2
zeoless <125026834+zeoless@users.noreply.github.com>
2021-06-21 16:14:49 +0000

    correct some typos

---
+++ internal/sql/ast/range_table_func.go
@@ -0,0 +1,16 @@
+
+package ast
+
+type RangeTableFunc struct {
+	Lateral    bool
+	Docexpr    Node
+	Rowexpr    Node
+	Namespaces *List
+	Columns    *List
+	Alias      *Alias
+	Location   int
+}
+
+func (n *RangeTableFunc) Pos() int {
+	return n.Location
+}
---
+++ internal/sql/ast/sec_label_stmt.go
@@ -0,0 +1,13 @@
+
+package ast
+
+type SecLabelStmt struct {
+	Objtype  ObjectType
+	Object   Node
+	Provider *string
+	Label    *string
+}
+
+func (n *SecLabelStmt) Pos() int {
+	return 0
+}
