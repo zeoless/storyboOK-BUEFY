4b421f46a19eef0b4768e04738e5b7b01dcb8e9b
zeoless <125026834+zeoless@users.noreply.github.com>
2021-06-23 12:49:17 +0000

    Update 01.md

--- internal/endtoend/testdata/ddl_comment/postgresql/pgx/v5/schema.sql
+++ internal/endtoend/testdata/ddl_comment/postgresql/pgx/v5/schema.sql
@@ -3,4 +3,5 @@ CREATE TABLE foo.bar (baz text);
 CREATE TYPE foo.bat AS ENUM ('bat');
 COMMENT ON SCHEMA foo IS 'Schema comment';
 COMMENT ON TABLE foo.bar IS 'Table comment';
-COMMENT ON COLUMN foo.bar.baz IS 'Co
+COMMENT ON COLUMN foo.bar.baz IS 'Column comment';
+COMMENT ON TYPE foo.bat IS 'Enum comment';
