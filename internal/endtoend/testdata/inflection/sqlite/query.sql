99fe6fe7111ca8a924e770bf0c913a5f8fb2c63f
zeoless <125026834+zeoless@users.noreply.github.com>
2021-06-24 08:37:08 +0000

    big fix

--- internal/endtoend/testdata/inflection/sqlite/query.sql
+++ internal/endtoend/testdata/inflection/sqlite/query.sql
@@ -1,4 +1,16 @@
 CREATE TABLE campus (id text not null);
 CREATE TABLE students (id text not null);
 CREATE TABLE product_meta (id text not null);
-CREATE TABLE
+CREATE TABLE calories (id text not null);
+
+/* name: ListCampuses :many */
+SELECT * FROM campus;
+
+/* name: ListStudents :many */
+SELECT * FROM students;
+
+/* name: ListMetadata :many */
+SELECT * FROM product_meta;
+
+/* name: ListCalories :many */
+SELECT * FROM calories;
