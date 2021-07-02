ab9d96b0ae1979ccbdc3ac0dde47f1d5c9eb03b9
zeoless <125026834+zeoless@users.noreply.github.com>
2021-07-02 09:20:18 +0000

    v1.4.5

--- examples/ondeck/mysql/venue.sql.go
+++ examples/ondeck/mysql/venue.sql.go
@@ -177,4 +177,17 @@ func (q *Queries) VenueCountByCity(ctx context.Context) ([]VenueCountByCityRow, 
 	defer rows.Close()
 	var items []VenueCountByCityRow
 	for rows.Next() {
-		var 
+		var i VenueCountByCityRow
+		if err := rows.Scan(&i.City, &i.Count); err != nil {
+			return nil, err
+		}
+		items = append(items, i)
+	}
+	if err := rows.Close(); err != nil {
+		return nil, err
+	}
+	if err := rows.Err(); err != nil {
+		return nil, err
+	}
+	return items, nil
+}
