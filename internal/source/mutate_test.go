5ae6e62bfdcc6ba1f7be27c1d24536edc0b1b923
zeoless <125026834+zeoless@users.noreply.github.com>
2021-07-30 16:26:15 +0000

    added content

--- internal/source/mutate_test.go
+++ internal/source/mutate_test.go
@@ -52,4 +52,136 @@ func TestMutateSingle(t *testing.T) {
 
 		// Raw insertions / deletions
 		{"abcde", newEdit(0, "", "_"), "_abcde"},
-		{"abcde", newEdit(1, "", "_"), "a_bcde"}
+		{"abcde", newEdit(1, "", "_"), "a_bcde"},
+		{"abcde", newEdit(2, "", "_"), "ab_cde"},
+		{"abcde", newEdit(3, "", "_"), "abc_de"},
+		{"abcde", newEdit(4, "", "_"), "abcd_e"},
+		{"abcde", newEdit(5, "", "_"), "abcde_"},
+	}
+
+	origTests := tests
+	// Generate the reverse mutations, for every edit - the opposite edit that makes it "undo"
+	for _, spec := range origTests {
+		tests = append(tests, test{
+			input:    spec.expected,
+			edit:     newEdit(spec.edit.Location, spec.edit.New, spec.edit.Old),
+			expected: spec.input,
+		})
+	}
+
+	for _, spec := range tests {
+		expected := spec.expected
+
+		actual, err := Mutate(spec.input, []Edit{spec.edit})
+		testName := fmt.Sprintf("Mutate(%s, Edit{%v, %v -> %v})", spec.input, spec.edit.Location, spec.edit.Old, spec.edit.New)
+		if err != nil {
+			t.Errorf("%s should not error (%v)", testName, err)
+			continue
+		}
+
+		if actual != expected {
+			t.Errorf("%s expected %v; got %v", testName, expected, actual)
+		}
+	}
+}
+
+// TestMutateMulti tests combinations of edits
+func TestMutateMulti(t *testing.T) {
+	type test struct {
+		input    string
+		edit1    Edit
+		edit2    Edit
+		expected string
+	}
+
+	tests := []test{
+		// Edits that are >1 character from each other
+		{"abcde", newEdit(0, "a", "A"), newEdit(2, "c", "C"), "AbCde"},
+		{"abcde", newEdit(0, "a", "A"), newEdit(2, "c", "C"), "AbCde"},
+
+		// 2 edits bump right up next to each other
+		{"abcde", newEdit(0, "abc", ""), newEdit(3, "de", "DE"), "DE"},
+		{"abcde", newEdit(0, "abc", "ABC"), newEdit(3, "de", ""), "ABC"},
+		{"abcde", newEdit(0, "abc", "ABC"), newEdit(3, "de", "DE"), "ABCDE"},
+		{"abcde", newEdit(1, "b", "BB"), newEdit(2, "c", "CC"), "aBBCCde"},
+
+		// 2 edits bump next to each other, but don't cover the whole string
+		{"abcdef", newEdit(1, "bc", "C"), newEdit(3, "de", "D"), "aCDf"},
+		{"abcde", newEdit(1, "bc", "CCCC"), newEdit(3, "d", "DDD"), "aCCCCDDDe"},
+
+		// lengthening edits
+		{"abcde", newEdit(1, "b", "BBBB"), newEdit(2, "c", "CCCC"), "aBBBBCCCCde"},
+	}
+
+	origTests := tests
+	// Generate the edits in opposite order mutations, source edits should be independent of
+	// the order the edits are specified
+	for _, spec := range origTests {
+		tests = append(tests, test{
+			input:    spec.input,
+			edit1:    spec.edit2,
+			edit2:    spec.edit1,
+			expected: spec.expected,
+		})
+	}
+
+	for _, spec := range tests {
+		expected := spec.expected
+
+		actual, err := Mutate(spec.input, []Edit{spec.edit1, spec.edit2})
+		testName := fmt.Sprintf("Mutate(%s, Edits{(%v, %v -> %v), (%v, %v -> %v)})", spec.input,
+			spec.edit1.Location, spec.edit1.Old, spec.edit1.New,
+			spec.edit2.Location, spec.edit2.Old, spec.edit2.New)
+
+		if err != nil {
+			t.Errorf("%s should not error (%v)", testName, err)
+			continue
+		}
+
+		if actual != expected {
+			t.Errorf("%s expected %v; got %v", testName, expected, actual)
+		}
+	}
+}
+
+// TestMutateErrorSingle test errors are generated for trivially incorrect single edits
+func TestMutateErrorSingle(t *testing.T) {
+	type test struct {
+		input string
+		edit  Edit
+	}
+
+	tests := []test{
+		// old text is longer than input text
+		{"", newEdit(0, "a", "A")},
+		{"a", newEdit(0, "aa", "A")},
+		{"hello", newEdit(0, "hello!", "A")},
+
+		// negative indexes
+		{"aaa", newEdit(-1, "aa", "A")},
+		{"aaa", newEdit(-2, "aa", "A")},
+		{"aaa", newEdit(-100, "aa", "A")},
+	}
+
+	for _, spec := range tests {
+		edit := spec.edit
+
+		_, err := Mutate(spec.input, []Edit{edit})
+		testName := fmt.Sprintf("Mutate(%s, Edit{%v, %v -> %v})", spec.input, edit.Location, edit.Old, edit.New)
+		if err == nil {
+			t.Errorf("%s should error (%v)", testName, err)
+			continue
+		}
+	}
+}
+
+// TestMutateErrorMulti tests error that can only happen across multiple errors
+func TestMutateErrorMulti(t *testing.T) {
+	type test struct {
+		input string
+		edit1 Edit
+		edit2 Edit
+	}
+
+	tests := []test{
+		// These edits overlap each other, and are therefore undefine
