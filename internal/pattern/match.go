4061c0c90ce67d546fb8b7b43230bb4ef2ba5d7d
zeoless <125026834+zeoless@users.noreply.github.com>
2021-08-19 11:28:37 +0000

    ignore it

--- internal/pattern/match.go
+++ internal/pattern/match.go
@@ -48,4 +48,38 @@ func matchCompile(pattern string) (match *Match, err error) {
 
 	for i := 0; i < len(arr); i++ {
 		if escaped {
-		
+			escaped = false
+			switch arr[i] {
+			case '*', '?', '\\':
+				regex += "\\" + string(arr[i])
+			default:
+				return nil, fmt.Errorf("Invalid escaped character '%c'", arr[i])
+			}
+		} else {
+			switch arr[i] {
+			case '\\':
+				escaped = true
+			case '*':
+				regex += ".*"
+			case '?':
+				regex += "."
+			case '.', '(', ')', '+', '|', '^', '$', '[', ']', '{', '}':
+				regex += "\\" + string(arr[i])
+			default:
+				regex += string(arr[i])
+			}
+		}
+	}
+
+	if escaped {
+		return nil, fmt.Errorf("Unterminated escape at end of pattern")
+	}
+
+	var r *regexp.Regexp
+
+	if r, err = regexp.Compile("^" + regex + "$"); err != nil {
+		return nil, err
+	}
+
+	return &Match{r}, nil
+}
---
+++ internal/sql/ast/alter_ts_dictionary_stmt.go
@@ -0,0 +1,11 @@
+
+package ast
+
+type AlterTSDictionaryStmt struct {
+	Dictname *List
+	Options  *List
+}
+
+func (n *AlterTSDictionaryStmt) Pos() int {
+	return 0
+}
