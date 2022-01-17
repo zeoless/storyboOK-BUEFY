ab1d6bf21608ef4019b6a36353611859c2690964
zeoless <125026834+zeoless@users.noreply.github.com>
2022-01-17 12:37:29 +0000

    [skip ci] Update README.md

--- internal/endtoend/testdata/insert_values/postgresql/pgx/v5/go/db.go
+++ internal/endtoend/testdata/insert_values/postgresql/pgx/v5/go/db.go
@@ -7,15 +7,14 @@ package querytest
 import (
 	"context"
 
-	"github.com/jackc/pgconn"
-	"github.com/jackc/pgx/v4"
+	"github.com/jackc/pgx/v5"
+	"github.com/jackc/pgx/v5/pgconn"
 )
 
 type DBTX interface {
 	Exec(context.Context, string, ...interface{}) (pgconn.CommandTag, error)
 	Query(context.Context, string, ...interface{}) (pgx.Rows, error)
 	QueryRow(context.Context, string, ...interface{}) pgx.Row
-	SendBatch(context.Context, *pgx.Batch) pgx.BatchResults
 }
 
 func New(db DBTX) *Queries {
