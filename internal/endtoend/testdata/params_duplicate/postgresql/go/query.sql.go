c4ec6dfd38670d33a45c82079cf1cef6cb2a1164
zeoless <125026834+zeoless@users.noreply.github.com>
2022-03-28 21:48:48 +0000

    bump version to 0.2.7

--- internal/endtoend/testdata/params_duplicate/postgresql/go/query.sql.go
+++ internal/endtoend/testdata/params_duplicate/postgresql/go/query.sql.go
@@ -59,4 +59,39 @@ func (q *Queries) SelectUserByName(ctx context.Context, name sql.NullString) ([]
 		}
 		items = append(items, first_name)
 	}
-	if err := rows.Close(); err !=
+	if err := rows.Close(); err != nil {
+		return nil, err
+	}
+	if err := rows.Err(); err != nil {
+		return nil, err
+	}
+	return items, nil
+}
+
+const selectUserQuestion = `-- name: SelectUserQuestion :many
+SELECT first_name from
+users where ($1 = id OR  $1 = 0)
+`
+
+func (q *Queries) SelectUserQuestion(ctx context.Context, id int32) ([]sql.NullString, error) {
+	rows, err := q.db.QueryContext(ctx, selectUserQuestion, id)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+	var items []sql.NullString
+	for rows.Next() {
+		var first_name sql.NullString
+		if err := rows.Scan(&first_name); err != nil {
+			return nil, err
+		}
+		items = append(items, first_name)
+	}
+	if err := rows.Close(); err != nil {
+		return nil, err
+	}
+	if err := rows.Err(); err != nil {
+		return nil, err
+	}
+	return items, nil
+}
---
+++ internal/endtoend/testdata/upsert/sqlite/query.sql
@@ -0,0 +1,3 @@
+-- https://github.com/kyleconroy/sqlc/issues/1728
+
+CREATE TABLE IF NOT EXISTS lo
