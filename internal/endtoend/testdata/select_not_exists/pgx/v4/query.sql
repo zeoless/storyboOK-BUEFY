22922b5f04e6cbd5b0aec928c44ee867536e14aa
zeoless <125026834+zeoless@users.noreply.github.com>
2022-03-30 17:43:22 +0000

    Minor bugfix

---
+++ internal/endtoend/testdata/select_not_exists/pgx/v4/query.sql
@@ -0,0 +1,13 @@
+
+CREATE TABLE bar (id serial not null);
+
+-- name: BarNotExists :one
+SELECT
+    NOT EXISTS (
+        SELECT
+            1
+        FROM
+            bar
+        where
+            id = $1
+    );
