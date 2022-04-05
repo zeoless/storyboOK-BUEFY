5ba3fca1a3e72a8797336178488d1c09d4d490f2
zeoless <125026834+zeoless@users.noreply.github.com>
2022-04-05 19:44:31 +0000

    bcma: add ext PA workaround for BCM4331 and BCM43431

--- examples/booktest/postgresql/db_test.go
+++ examples/booktest/postgresql/db_test.go
@@ -86,3 +86,72 @@ func TestBooks(t *testing.T) {
 	}
 
 	// save fourth book
+	b3, err := tq.CreateBook(ctx, CreateBookParams{
+		AuthorID:  a.AuthorID,
+		Isbn:      "4",
+		Title:     "4th place finisher",
+		BookType:  BookTypeNONFICTION,
+		Year:      2011,
+		Available: now,
+		Tags:      []string{"other"},
+	})
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// tx commit
+	err = tx.Commit()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// upsert, changing ISBN and title
+	err = dq.UpdateBookISBN(ctx, UpdateBookISBNParams{
+		BookID: b3.BookID,
+		Isbn:   "NEW ISBN",
+		Title:  "never ever gonna finish, a quatrain",
+		Tags:   []string{"someother"},
+	})
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// retrieve first book
+	books0, err := dq.BooksByTitleYear(ctx, BooksByTitleYearParams{
+		Title: "my book title",
+		Year:  2016,
+	})
+	if err != nil {
+		t.Fatal(err)
+	}
+	for _, book := range books0 {
+		t.Logf("Book %d (%s): %s available: %s\n", book.BookID, book.BookType, book.Title, book.Available.Format(time.RFC822Z))
+		author, err := dq.GetAuthor(ctx, book.AuthorID)
+		if err != nil {
+			t.Fatal(err)
+		}
+		t.Logf("Book %d author: %s\n", book.BookID, author.Name)
+	}
+
+	// find a book with either "cool" or "other" tag
+	t.Logf("---------\nTag search results:\n")
+	res, err := dq.BooksByTags(ctx, []string{"cool", "other", "someother"})
+	if err != nil {
+		t.Fatal(err)
+	}
+	for _, ab := range res {
+		t.Logf("Book %d: '%s', Author: '%s', ISBN: '%s' Tags: '%v'\n", ab.BookID, ab.Title, ab.Name.String, ab.Isbn, ab.Tags)
+	}
+
+	// TODO: call say_hello(varchar)
+
+	// get book 4 and delete
+	b5, err := dq.GetBook(ctx, b3.BookID)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if err := dq.DeleteBook(ctx, b5.BookID); err != nil {
+		t.Fatal(err)
+	}
+
+}
