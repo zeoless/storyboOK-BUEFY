653363dcfb96c8a6ba43367ca5a13f62d14d6621
zeoless <125026834+zeoless@users.noreply.github.com>
2022-04-07 11:20:55 +0000

    [ca] new rules

--- internal/endtoend/testdata/insert_values/sqlite/go/query.sql.go
+++ internal/endtoend/testdata/insert_values/sqlite/go/query.sql.go
@@ -5,4 +5,21 @@
 
 package querytest
 
-imp
+import (
+	"context"
+	"database/sql"
+)
+
+const insertValues = `-- name: InsertValues :exec
+INSERT INTO foo (a, b) VALUES (?, ?)
+`
+
+type InsertValuesParams struct {
+	A sql.NullString
+	B sql.NullInt64
+}
+
+func (q *Queries) InsertValues(ctx context.Context, arg InsertValuesParams) error {
+	_, err := q.db.ExecContext(ctx, insertValues, arg.A, arg.B)
+	return err
+}
