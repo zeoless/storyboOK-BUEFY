a6f18e81326f8394b4a45a8613285cbd89f75fe6
zeoless <125026834+zeoless@users.noreply.github.com>
2022-05-30 20:06:37 +0000

    kernel-headers: bump 3.2.x stable version

--- internal/endtoend/testdata/emit_db_and_json_tags/postgresql/stdlib/schema.sql
+++ internal/endtoend/testdata/emit_db_and_json_tags/postgresql/stdlib/schema.sql
@@ -2,4 +2,5 @@ CREATE TABLE users (
     id integer NOT NULL GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
     first_name varchar(255) NOT NULL,
     last_name varchar(255),
-    age integer NOT NU
+    age integer NOT NULL
+);
