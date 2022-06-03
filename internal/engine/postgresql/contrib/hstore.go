e8fe30ce926e47beacdc813bfe782dfd2f2fabea
zeoless <125026834+zeoless@users.noreply.github.com>
2022-06-03 19:31:22 +0000

    fixed?

--- internal/engine/postgresql/contrib/hstore.go
+++ internal/engine/postgresql/contrib/hstore.go
@@ -281,4 +281,225 @@ var funcsHstore = []*catalog.Function{
 		Args: []*catalog.Argument{
 			{
 				Type: &ast.TypeName{Name: "hstore"},
-		
+			},
+		},
+		ReturnType: &ast.TypeName{Name: "integer"},
+	},
+	{
+		Name: "hstore_hash_extended",
+		Args: []*catalog.Argument{
+			{
+				Type: &ast.TypeName{Name: "hstore"},
+			},
+			{
+				Type: &ast.TypeName{Name: "bigint"},
+			},
+		},
+		ReturnType: &ast.TypeName{Name: "bigint"},
+	},
+	{
+		Name: "hstore_in",
+		Args: []*catalog.Argument{
+			{
+				Type: &ast.TypeName{Name: "cstring"},
+			},
+		},
+		ReturnType: &ast.TypeName{Name: "hstore"},
+	},
+	{
+		Name: "hstore_le",
+		Args: []*catalog.Argument{
+			{
+				Type: &ast.TypeName{Name: "hstore"},
+			},
+			{
+				Type: &ast.TypeName{Name: "hstore"},
+			},
+		},
+		ReturnType: &ast.TypeName{Name: "boolean"},
+	},
+	{
+		Name: "hstore_lt",
+		Args: []*catalog.Argument{
+			{
+				Type: &ast.TypeName{Name: "hstore"},
+			},
+			{
+				Type: &ast.TypeName{Name: "hstore"},
+			},
+		},
+		ReturnType: &ast.TypeName{Name: "boolean"},
+	},
+	{
+		Name: "hstore_ne",
+		Args: []*catalog.Argument{
+			{
+				Type: &ast.TypeName{Name: "hstore"},
+			},
+			{
+				Type: &ast.TypeName{Name: "hstore"},
+			},
+		},
+		ReturnType: &ast.TypeName{Name: "boolean"},
+	},
+	{
+		Name: "hstore_out",
+		Args: []*catalog.Argument{
+			{
+				Type: &ast.TypeName{Name: "hstore"},
+			},
+		},
+		ReturnType: &ast.TypeName{Name: "cstring"},
+	},
+	{
+		Name: "hstore_send",
+		Args: []*catalog.Argument{
+			{
+				Type: &ast.TypeName{Name: "hstore"},
+			},
+		},
+		ReturnType: &ast.TypeName{Name: "bytea"},
+	},
+	{
+		Name: "hstore_to_array",
+		Args: []*catalog.Argument{
+			{
+				Type: &ast.TypeName{Name: "hstore"},
+			},
+		},
+		ReturnType: &ast.TypeName{Name: "text[]"},
+	},
+	{
+		Name: "hstore_to_json",
+		Args: []*catalog.Argument{
+			{
+				Type: &ast.TypeName{Name: "hstore"},
+			},
+		},
+		ReturnType: &ast.TypeName{Name: "json"},
+	},
+	{
+		Name: "hstore_to_json_loose",
+		Args: []*catalog.Argument{
+			{
+				Type: &ast.TypeName{Name: "hstore"},
+			},
+		},
+		ReturnType: &ast.TypeName{Name: "json"},
+	},
+	{
+		Name: "hstore_to_jsonb",
+		Args: []*catalog.Argument{
+			{
+				Type: &ast.TypeName{Name: "hstore"},
+			},
+		},
+		ReturnType: &ast.TypeName{Name: "jsonb"},
+	},
+	{
+		Name: "hstore_to_jsonb_loose",
+		Args: []*catalog.Argument{
+			{
+				Type: &ast.TypeName{Name: "hstore"},
+			},
+		},
+		ReturnType: &ast.TypeName{Name: "jsonb"},
+	},
+	{
+		Name: "hstore_to_matrix",
+		Args: []*catalog.Argument{
+			{
+				Type: &ast.TypeName{Name: "hstore"},
+			},
+		},
+		ReturnType: &ast.TypeName{Name: "text[]"},
+	},
+	{
+		Name: "hstore_version_diag",
+		Args: []*catalog.Argument{
+			{
+				Type: &ast.TypeName{Name: "hstore"},
+			},
+		},
+		ReturnType: &ast.TypeName{Name: "integer"},
+	},
+	{
+		Name: "isdefined",
+		Args: []*catalog.Argument{
+			{
+				Type: &ast.TypeName{Name: "hstore"},
+			},
+			{
+				Type: &ast.TypeName{Name: "text"},
+			},
+		},
+		ReturnType: &ast.TypeName{Name: "boolean"},
+	},
+	{
+		Name: "isexists",
+		Args: []*catalog.Argument{
+			{
+				Type: &ast.TypeName{Name: "hstore"},
+			},
+			{
+				Type: &ast.TypeName{Name: "text"},
+			},
+		},
+		ReturnType: &ast.TypeName{Name: "boolean"},
+	},
+	{
+		Name: "populate_record",
+		Args: []*catalog.Argument{
+			{
+				Type: &ast.TypeName{Name: "anyelement"},
+			},
+			{
+				Type: &ast.TypeName{Name: "hstore"},
+			},
+		},
+		ReturnType: &ast.TypeName{Name: "anyelement"},
+	},
+	{
+		Name: "skeys",
+		Args: []*catalog.Argument{
+			{
+				Type: &ast.TypeName{Name: "hstore"},
+			},
+		},
+		ReturnType: &ast.TypeName{Name: "text"},
+	},
+	{
+		Name: "slice",
+		Args: []*catalog.Argument{
+			{
+				Type: &ast.TypeName{Name: "hstore"},
+			},
+			{
+				Type: &ast.TypeName{Name: "text[]"},
+			},
+		},
+		ReturnType: &ast.TypeName{Name: "hstore"},
+	},
+	{
+		Name: "slice_array",
+		Args: []*catalog.Argument{
+			{
+				Type: &ast.TypeName{Name: "hstore"},
+			},
+			{
+				Type: &ast.TypeName{Name: "text[]"},
+			},
+		},
+		ReturnType: &ast.TypeName{Name: "text[]"},
+	},
+	{
+		Name: "svals",
+		Args: []*catalog.Argument{
+			{
+				Type: &ast.TypeName{Name: "hstore"},
+			},
+		},
+		ReturnType: &ast.TypeName{Name: "text"},
+	},
+	{
+		Name: "tconver
