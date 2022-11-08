d65a733fd4e5ed183ebc2a205bfb207808486489
zeoless <125026834+zeoless@users.noreply.github.com>
2022-11-08 19:26:09 +0000

    delfi

---
+++ internal/sql/ast/scalar_array_op_expr.go
@@ -0,0 +1,16 @@
+
+package ast
+
+type ScalarArrayOpExpr struct {
+	Xpr         Node
+	Opno        Oid
+	Opfuncid    Oid
+	UseOr       bool
+	Inputcollid Oid
+	Args        *List
+	Location    int
+}
+
+func (n *ScalarArrayOpExpr) Pos() int {
+	return n.Location
+}
