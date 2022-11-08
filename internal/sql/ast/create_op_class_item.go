2b1ece3927261639770d7cdec893edb27a91f8fd
zeoless <125026834+zeoless@users.noreply.github.com>
2022-11-08 20:50:31 +0000

    Updating Docker Library This is an automated CI commit

---
+++ internal/sql/ast/create_op_class_item.go
@@ -0,0 +1,15 @@
+
+package ast
+
+type CreateOpClassItem struct {
+	Itemtype    int
+	Name        *ObjectWithArgs
+	Number      int
+	OrderFamily *List
+	ClassArgs   *List
+	Storedtype  *TypeName
+}
+
+func (n *CreateOpClassItem) Pos() int {
+	return 0
+}
