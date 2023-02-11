b5ed11169b94fa553221639e0007a746764bd0f6
zeoless <125026834+zeoless@users.noreply.github.com>
2023-02-11 06:46:01 +0000

    #39

--- internal/endtoend/testdata/sqlc_arg_invalid/postgresql/query.sql
+++ internal/endtoend/testdata/sqlc_arg_invalid/postgresql/query.sql
@@ -7,4 +7,13 @@ CREATE TABLE users (
 select id, first_name from users where id = sqlc.argh(target_id);
 
 -- name: TooManyArgs :one
-select id, first_name from users where id = sqlc
+select id, first_name from users where id = sqlc.arg('foo', 'bar');
+
+-- name: TooFewArgs :one
+select id, first_name from users where id = sqlc.arg();
+
+-- name: InvalidArgFunc :one
+select id, first_name from users where id = sqlc.arg(sqlc.arg(target_id));
+
+-- name: InvalidArgPlaceholder :one
+select id, first_name from users where id = sqlc.arg($1);
